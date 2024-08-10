a430670cc326c7669b3405f02a31e439ebd7f87b
le_blyad <131721618+le-blyad@users.noreply.github.com>
2024-08-11 00:36:40 +0700

    создал вход

---
+++ app/src/main/java/com/example/pokushai_mobile/LogIn.kt
@@ -0,0 +1,25 @@
+package com.example.pokushai_mobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        val buttonBack = findViewById<ImageButton>(R.id.buttonBack)
+        buttonBack.setOnClickListener {
+            onBackPressed()
+        }
+
+        val logOut = findViewById<TextView>(R.id.logOut)
+        logOut.setOnClickListener {
+            val intent = Intent(this, LogOut::class.java)
+            startActivity(intent)
+        }
+    }
+}
