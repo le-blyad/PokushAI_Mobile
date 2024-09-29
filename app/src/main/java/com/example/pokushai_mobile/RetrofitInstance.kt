cc7f36731500f5c39c5cfbeec3c3861c2440c0d7
le_blyad <131721618+le-blyad@users.noreply.github.com>
2024-09-30 01:44:49 +0700

    изменил для более удобного использования файла

--- app/src/main/java/com/example/pokushai_mobile/RetrofitInstance.kt
+++ app/src/main/java/com/example/pokushai_mobile/RetrofitInstance.kt
@@ -1,11 +1,16 @@
 package com.example.pokushai_mobile
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val retrofit = Retrofit.Builder()
-    .baseUrl("http://172.20.10.2:8000/")  // Адрес Django-сервера
-    .addConverterFactory(GsonConverterFactory.create())
-    .build()
+object ApiClient {
+    private const val BASE_URL = "http://172.20.10.2:8000/"
 
-val apiService = retrofit.create(ApiService::class.java)
-
+    val instance: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
