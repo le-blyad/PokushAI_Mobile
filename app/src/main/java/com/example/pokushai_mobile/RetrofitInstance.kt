f665d5deb728ded51b8862daada18a1523e0d5fe
le_blyad <131721618+le-blyad@users.noreply.github.com>
2024-11-17 20:39:49 +0700

    вот пуш с ссылкой

--- app/src/main/java/com/example/pokushai_mobile/RetrofitInstance.kt
+++ app/src/main/java/com/example/pokushai_mobile/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://192.168.1.42:8000/"
+    private const val BASE_URL = "https://pokushai.pythonanywhere.com/"
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
