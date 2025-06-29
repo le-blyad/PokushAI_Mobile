860a80697fdb6ac5f105d01d4c15294f5c9270a1
le_blyad <131721618+le-blyad@users.noreply.github.com>
2025-06-29 20:57:20 +0700

    начал реализизацию через фрагменты, пока приложение просто вылетает

---
+++ app/src/main/java/com/example/pokushai_mobile/MainFragment.kt
@@ -0,0 +1,16 @@
+package com.example.pokushai_mobile
+
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.fragment.app.Fragment
+import org.tensorflow.lite.Interpreter
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+
+}
